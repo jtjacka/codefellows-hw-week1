dbb53cf659bca22e1ea4ff609715fe8eec58212c
Jeffrey Jacka <jjacka@customplusdistributing.com>
2015-08-03 19:53:44 -0700

    Fill TableView with Tweet Text

--- TwitterCF/TwitterCF/ViewController.swift
+++ TwitterCF/TwitterCF/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var tableView: UITableView!
   
   var tweets = [Tweet]()
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    tableView.dataSource = self
+    
     
     if let filepath = NSBundle.mainBundle().pathForResource("tweet", ofType: "json") {
       if let data = NSData(contentsOfFile: filepath) {
@@ -35,3 +39,22 @@ class ViewController: UIViewController {
 
 }
 
+
+extension ViewController : UITableViewDataSource {
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return tweets.count
+  }
+  
+  
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("PostCell", forIndexPath: indexPath) as! UITableViewCell
+    let tweet = tweets[indexPath.row]
+    
+    cell.textLabel?.text = tweet.text
+    
+    return cell
+  }
+  
+
+}
