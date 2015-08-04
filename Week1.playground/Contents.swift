7de144c00b71a93bde516173879e1491375a40b7
Jeffrey Jacka <jjacka@customplusdistributing.com>
2015-08-04 08:54:48 -0700

    Add Tuesday Code Challenege

--- Week1.playground/Contents.swift
+++ Week1.playground/Contents.swift
@@ -17,5 +17,15 @@ for i in 0..<(arrayToReverse.count/2) {
 }
 
 arrayToReverse
-
 // MARK: Tuesday
+for i in 1...100 {
+  if ((i % 3 == 0)  && (i % 5 == 0)){
+    println("FizzBuzz")
+  } else if (i % 3 == 0) {
+    println("Fizz")
+  } else if (i % 5 == 0) {
+    println("Buzz")
+  } else {
+    //Do nothing
+  }
+}
